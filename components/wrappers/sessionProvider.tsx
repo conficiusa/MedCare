5ae77aea020502f9e2c5e8b05c61fd8d0947feec
Conficius <116452045+conficiusa@users.noreply.github.com>
2024-10-23 10:19:45 +0000

    Delete components directory

--- components/wrappers/sessionProvider.tsx
+++
@@ -1,6 +0,0 @@
-import { SessionProvider } from "next-auth/react";
-const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
-
-export default AuthProvider;
