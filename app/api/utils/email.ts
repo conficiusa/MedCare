eebad0ffbd93c4b493d74e7b13b6304fbf049dc1
conficiusa <addawebadua@gmail.com>
2024-12-31 15:56:31 +0000

    Refactor email transport configuration: add port setting and improve function formatting

--- app/api/utils/email.ts
+++ app/api/utils/email.ts
@@ -1,16 +1,19 @@
 "use server";
 import nodemailer from "nodemailer";
 
-
 const transporter = nodemailer.createTransport({
   service: "gmail",
-
+  port: 465,
   auth: {
     user: "addawebadua@gmail.com",
     pass: process.env.GMAIL_PASSWORD, // Make sure to use an app-specific password or OAuth2 for better security
   },
 });
- export async function sendEmail(recipient: string, subject: string, body: string) {
+export async function sendEmail(
+  recipient: string,
+  subject: string,
+  body: string
+) {
   try {
     // Define the email options
     const mailOptions = {
